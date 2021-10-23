f78fd3ef291ac27b4003a253870124b9dbe57386
Peter Hui <huishikuen@gmail.com>
2021-10-23 14:56:55 -0700

    used Google API

---
+++ findImageLabels.js
@@ -0,0 +1,19 @@
+async function findImageLabels() {
+    // Imports the Google Cloud client library
+    const vision = require('@google-cloud/vision');
+  
+    // Creates a client
+    const client = new vision.ImageAnnotatorClient();
+  
+    // Performs label detection on the image file
+    const [result] = await client.labelDetection('./resources/wakeupcat.jpeg');
+    const labels = result.labelAnnotations;
+    console.log('Labels:');
+    const labelNames = [];
+    labels.forEach(label => labelNames.push(label.description));
+    return labelNames
+  }
+  
+module.exports = {
+  findImageLabels
+}
